f414cd812f3a4561219d029a966826889250835a
Karen Payne <paynekaren@comcast.net>
2019-10-03 04:34:06 -0700

    Add code sample for those who care not to use a library

---
+++ MinimalApproachExample/DatabaseUser.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Security;
+
+namespace MinimalApproachExample
+{
+    public class DatabaseUser
+    {
+        private string serverName;
+        private string catalogName;
+        public DatabaseUser(string pServerName, string pCatalogName)
+        {
+            serverName = pServerName;
+            catalogName = pCatalogName;
+        }
+        public bool SqlCredentialLogin(string pUserNName, string pPassword)
+        {
+
+            string connectionString = $"Data Source={serverName};" +
+                                      $"Initial Catalog={catalogName};";
+
+
+            var securePassword = new SecureString();
+
+            foreach (var character in pPassword)
+            {
+                securePassword.AppendChar(character);
+            }
+
+            securePassword.MakeReadOnly();
+
+            var credentials = new SqlCredential(pUserNName, securePassword);
+
+            using (var cn = new SqlConnection { ConnectionString = connectionString })
+            {
+                try
+                {
+                    cn.Credential = credentials;
+                    cn.Open();
+                    return true;
+
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+        public string DoWork(string pUserNName, string pPassword)
+        {
+
+            string connectionString = $"Data Source={serverName};" +
+                                      $"Initial Catalog={catalogName};";
+
+
+            var securePassword = new SecureString();
+
+            foreach (var character in pPassword)
+            {
+                securePassword.AppendChar(character);
+            }
+
+            securePassword.MakeReadOnly();
+
+            var credentials = new SqlCredential(pUserNName, securePassword);
+
+            using (var cn = new SqlConnection { ConnectionString = connectionString })
+            {
+                try
+                {
+                    cn.Credential = credentials;
+                    cn.Open();
+                    // do work here
+                    return "";
+                }
+                catch (Exception e)
+                {
+                    return e.Message;
+                }
+            }
+        }
+    }
+}
---
+++ MinimalApproachExample/LoginForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MinimalApproachExample
+{
+	public partial class LoginForm
+	{
+		public LoginForm()
+		{
+			InitializeComponent();
+		}
+		private void ShowHidePasswordCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            PasswordTextBox.PasswordChar = ShowHidePasswordCheckBox.Checked ? '*' : '\0';
+        }
+		private void LoginButton_Click(object sender, EventArgs e)
+		{
+
+			if (!string.IsNullOrWhiteSpace(UserNameTextBox.Text) && !string.IsNullOrWhiteSpace(PasswordTextBox.Text))
+			{
+
+				var ops = new DatabaseUser("KARENS-PC", "UserLoginExample"); 
+
+                // perform login
+                var loginResults = ops.SqlCredentialLogin(UserNameTextBox.Text, PasswordTextBox.Text);
+
+                if (loginResults)
+                {
+
+                    var successValue = ops.DoWork(UserNameTextBox.Text, PasswordTextBox.Text);
+                    var workResult = string.IsNullOrWhiteSpace(successValue);
+
+                    if (workResult)
+                    {
+                        // operation was successful
+                        MessageBox.Show("Work complete");
+                    }
+                    else
+                    {
+                        // operation has failed
+                        MessageBox.Show(successValue);
+                    }
+                }
+                else
+                {
+                    // login failed for one or more reasons
+                    MessageBox.Show("Login failed");
+                }
+			}
+			else
+			{
+                // missing user name and or user password
+				MessageBox.Show("Incomplete information to continue.");
+			}
+		}
+		private void CancelButton_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
+}
